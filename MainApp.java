97312ea1f9244a417dd0b26f2d50f23dbf5b5c80
Marques Fabien <fabienmrqs@gmail.com>
2023-06-01 10:28:48 +0200

    finish operator

--- MainApp.java
+++ MainApp.java
@@ -4,8 +4,15 @@ public class MainApp {
         int ageUtilisateur = 10;
         final int NUMB = 15;
         int result = 5 + 1;
+
+
+        int exp = 0;
+        exp = exp + 10;
+        exp += 10;
+
+        
         System.out.println("Tu as " + ageUtilisateur + " ans");
-        System.out.println(result);
+        System.out.println(exp);
 
     }
 }
